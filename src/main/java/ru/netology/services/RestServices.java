e361021e1725d2360e6408fa6a406cc4c88bffa4
Alex Neko <128819829+AlexIvanochkin@users.noreply.github.com>
2024-02-20 10:15:16 +0300

    changes

--- src/main/java/ru/netology/services/RestServices.java
+++ src/main/java/ru/netology/services/RestServices.java
@@ -3,19 +3,19 @@ package ru.netology.services;
     public class RestServices {
         public int RestServices(int income, int expenses, int threshold) {
 
-            int x = 0;// деньги
-            int y = 0; //сбережения
+            int money = 0;// деньги
+            int capital = 0; //сбережения
             //int z = 1; //месяцы
 
-            for (int z = 1; z < 12; z++) {
-                if (x >= threshold) {
-                    y++;
-                    x = (x - expenses) / 3;
+            for (int i = 1; i < 12; i++) {
+                if (money >= threshold) {
+                    capital++;
+                    money = (money - expenses) / 3;
                 } else {
 
-                    x = x + income - expenses ;
+                    money = money + income - expenses ;
                 }
             }
-            return y;
+            return capital;
         }
     }
